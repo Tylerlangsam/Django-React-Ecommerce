6269d86042f2e71f476649104728dedc3468869f
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-05-11 10:55:10 -0700

    Setting up product app

---
+++ products/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
